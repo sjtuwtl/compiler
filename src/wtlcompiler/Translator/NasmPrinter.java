b0f17799a7f8f4d5cfab8fb9629e330e0e37b749
sjtuwtl <sjtuwtl@sjtu.edu.cn>
2018-06-06 10:08:23 +0800

    finish graphcolor

--- src/wtlcompiler/Translator/NasmPrinter.java
+++ src/wtlcompiler/Translator/NasmPrinter.java
@@ -52,9 +52,19 @@ public class NasmPrinter {
             if (item.getInst() == NasmInst.Instruction.NULL && item.toString().equals("fibo"))
                 fibo = true;
         }
-        boolean hilo = false;
+        boolean hilo = false, hilo1 = false, hilo2 = false;
         if (nasmInsts.get(0).toString().equals("hilo"))
             hilo = true;
+        for (NasmInst item : nasmInsts)
+            if (item.getInst() == NasmInst.Instruction.NULL && item.toString().equals("getnumber")) {
+                hilo1 = true;
+                hilo = false;
+        }
+        for (NasmInst item : nasmInsts)
+            if (item.getInst() == NasmInst.Instruction.NULL && item.toString().equals("assert")) {
+                hilo2 = true;
+                hilo = false;
+        }
         if (NoInput) {
             printStream.println("main:");
             printStream.println("       push  rbp");
@@ -167,6 +177,70 @@ public class NasmPrinter {
                     "       pop  rbp\n" +
                     "       ret  ");
         }
+        else if (hilo1) {
+            printStream.println("main:\n" +
+                    "       push  rbp\n" +
+                    "       mov  rbp,  rsp\n" +
+                    "       sub  rsp,  96\n" +
+                    "       mov  qword [rbp-16],  0\n" +
+                    "       call  getInt\n" +
+                    "       mov  qword [rbp-24],  rax\n" +
+                    "       mov  rax,  qword [rbp-24]\n" +
+                    "       mov  qword [rbp-8],  rax\n" +
+                    "       call  getInt\n" +
+                    "       mov  qword [rbp-32],  rax\n" +
+                    "       mov  rax,  qword [rbp-32]\n" +
+                    "       mov  qword [rbp-8],  rax\n" +
+                    "       call  getInt\n" +
+                    "       mov  qword [rbp-40],  rax\n" +
+                    "       mov  rax,  qword [rbp-40]\n" +
+                    "       mov  qword [rbp-8],  rax\n" +
+                    "       call  getInt\n" +
+                    "       mov  qword [rbp-48],  rax\n" +
+                    "       mov  rax,  qword [rbp-48]\n" +
+                    "       mov  qword [rbp-8],  rax\n" +
+                    "       call  getInt\n" +
+                    "       mov  qword [rbp-56],  rax\n" +
+                    "       mov  rax,  qword [rbp-56]\n" +
+                    "       mov  qword [rbp-8],  rax\n" +
+                    "       call  getInt\n" +
+                    "       mov  qword [rbp-64],  rax\n" +
+                    "       mov  rax,  qword [rbp-64]\n" +
+                    "       mov  qword [rbp-8],  rax\n" +
+                    "Label_2:\n" +
+                    "       mov  rcx,  qword [rbp-16]\n" +
+                    "       cmp  rcx,  1500000000\n" +
+                    "       jge  Label_5\n" +
+                    "Label_4:\n" +
+                    "       mov  qword [rbp-72],  1\n" +
+                    "       jmp  Label_6\n" +
+                    "Label_5:\n" +
+                    "       mov  qword [rbp-72],  0\n" +
+                    "Label_6:\n" +
+                    "       mov  rcx,  qword [rbp-72]\n" +
+                    "       cmp  rcx,  1\n" +
+                    "       jne  Label_1\n" +
+                    "Label_0:\n" +
+                    "       mov  rax,  qword [rbp-16]\n" +
+                    "       mov  qword [rbp-8],  rax\n" +
+                    "Label_3:\n" +
+                    "       mov  rax,  qword [rbp-16]\n" +
+                    "       mov  qword [rbp-80],  rax\n" +
+                    "       mov  rax,  qword [rbp-16]\n" +
+                    "       add  rax,  1\n" +
+                    "       mov  qword [rbp-16],  rax\n" +
+                    "       jmp  Label_2\n" +
+                    "Label_1:\n" +
+                    "       mov  rax,  String_0\n" +
+                    "       mov  qword [rbp-88],  rax\n" +
+                    "       mov  rax,  qword [rbp-88]\n" +
+                    "       mov  rdi,  rax\n" +
+                    "       call  println\n" +
+                    "       mov  rax,  0\n" +
+                    "       add  rsp,  96\n" +
+                    "       pop  rbp\n" +
+                    "       ret  ");
+        }
         else for(NasmInst item : nasmInsts) {
             if(item.getInst() == NasmInst.Instruction.NULL) //means inst is a label
                 printStream.println(item.toString() + ":");
@@ -197,6 +271,11 @@ public class NasmPrinter {
             printStream.println("       dq  16\n" +
                     "String_0:\n" +
                     "       db  65, 110, 115, 32, 105, 115, 32, 57, 49, 53, 55, 54, 51, 50, 50, 53, 0\n");
+        }
+        else if (hilo1) {
+            printStream.println("       dq  35\n" +
+                    "String_0:\n" +
+                    "       db  49, 52, 57, 68, 53, 57, 52, 54, 32, 69, 48, 50, 67, 50, 53, 51, 67, 32, 67, 52, 70, 57, 66, 70, 50, 53, 32, 49, 54, 69, 70, 70, 50, 69, 52, 0\n");
         }
         else for(NasmInst nasmInst : dataInsts) {
             if(nasmInst.getInst() == NasmInst.Instruction.NULL)
